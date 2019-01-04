57ca2f7f3145bcef12f6e7a6f38b520e1cc31649
HaiderZaidiDev <45272003+HaiderZaidiDev@users.noreply.github.com>
2019-01-03 22:51:35 -0500

    Updated +servers
    Updated documentation for +servers and fixed variable error.

--- main.py
+++ main.py
@@ -20,8 +20,10 @@ async def on_ready(): # Defines event response. (Executes on successful join).
 @client.event # Defining client event. 
 async def on_message(message): # Defines event response. (Executes on message.)
     if message.author != client.user: # If the message isn't sent by the bot, the following code is executed. 
-      if message.content.startswith('+servers'):
-        await client.send_message(message.channel, ''' I'm currently being used in''' + str(serverCount) + 'server(s)') 
+      
+      if message.content.startswith('+servers'): #  If the user enters +servers, the following code is executed. 
+        serverCount = len(client.servers) # Number of servers the bot is currently in. 
+        await client.send_message(message.channel, ''' I'm currently being used in''' + str(serverCount) + 'server(s)') # Bot output. 
         
       if message.content.startswith('+ping'): # If the user enters +ping, the following code is executed.
         await client.send_message(message.channel, 'Pong!')
